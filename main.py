c7d5ebe7b67ba680b0d1161c16049af1e1298190
cbspears <cbspears@gmail.com>
2026-01-06 23:34:42 -0600

    Fix missing app definition

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ import matplotlib.patches as mpatches
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 import psycopg
+
 from psycopg.rows import dict_row
+
+app = Flask(__name__)
 
 
 # Slack setup
